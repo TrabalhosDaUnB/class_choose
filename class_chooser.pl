166ed7426162dacf41e4c94ffeaf32e339be9991
Lucas S <lucas.soaresouza@gmail.com>
2017-09-29 03:33:31 -0300

    Adicionando perguntas para selecao dos atributos de classe

--- class_chooser.pl
+++ class_chooser.pl
@@ -1,13 +1,56 @@
-%definicao das classes dos chars
-%a definicao foi feita em cima de 4 atributos forca, inteligencia, destreza e vitalidade
-%sendo que inteligencia e forca se anulam
-
 
 %forca combinado a destreza
-qual_eh_a_classe(for, dex, cavaleiro).
-qual_eh_a_classe(for, vit, guerreiro).
-qual_eh_a_classe(int, dex, arqueiro).
-qual_eh_a_classe(int, vit, mago).
-qual_eh_a_classe(vit, dex, assassino).
-qual_eh_a_classe(for, int, inexistente).
+atributos(forca, destreza).
+atributos(forca, vitalidade).
+atributos(inteligencia, destreza).
+atributos(inteligencia, vitalidade).
+atributos(destreza, destreza).
 
+eh_classe(cavaleiro) :- atributos(forca, destreza).
+eh_classe(guerreiro) :- atributos(forca, vitalidade).
+eh_classe(arqueiro) :- atributos(inteligencia, destreza).
+eh_classe(mago) :- atributos(inteligencia, vitalidade).
+eh_classe(assassino) :- atributos(destreza, destreza).
+
+iniciar :- repeat,
+    write("=======Seletor_de_Classe======="), nl,
+    write("1. Vamos iniciar!"), nl,
+    write("2. Me tire daqui!"), nl,
+    read(X),
+    opcao(X),
+    X==2, !.
+
+opcao(2) :- !.
+opcao(1) :- pergunta_1.
+
+pergunta_1 :-
+    write("=======Seletor_de_Classe======="), nl,
+    write("Voce estava viajando pelas terras de Javalovisk e encontrou um Dragon NullPointerException. Então você:"), nl,
+    write("1. Com muito odio explode a terra de Javalovisk com o poder da mente"), nl,
+    write("2. Com as proprias maos destroi o Dragon NullPointerException"), nl,
+    write("3. Se acalma e telepaticamente entra em contato com o mago StackOverflow que te encontra a solucao"), nl,
+    write("4. Voce utiliza uma habilidade especial e reduz o Dragon a cinzas"), nl,
+    write("5. O Dragon nao apareceria para voce pois voce nao anda em Javalovisk."), nl,
+    read(X),
+    opc_pergunta_1(X).
+
+opc_pergunta_1(1) :- pergunta_2(inteligencia).
+opc_pergunta_1(2) :- pergunta_2(forca).
+opc_pergunta_1(3) :- pergunta_2(inteligencia).
+opc_pergunta_1(4) :- pergunta_2(forca).
+opc_pergunta_1(5) :- pergunta_2(destreza).
+
+pergunta_2(X) :-
+    write("=======Seletor_de_Classe======="), nl,
+    write("Voce encontrou o livro do mage ByShaQui e ao tentar fazer um feitico voce evocou o gnomo BahIhDu e ele invade seu computador! O que voce faz?"), nl,
+    write("1. Converso com ele até me acostumar com sua presenca"), nl,
+    write("2. Uso minhas habilidades especiais de sobrinho formatador de pc da familia e detono o gnomo"), nl,
+    write("3. Meu pc é sinistro um gnomo a mais outro a menos não faz diferenca."), nl,
+    write("4. Nao aconteceria pois voce sabe que o livro de ByShaQui eh escuro e cheio de horrores!"), nl,
+    read(Y),
+    opc_pergunta_2(X,Y).
+
+opc_pergunta_2(X,1) :- write(atributos(X,vitalidade)), nl.
+opc_pergunta_2(X,2) :- write(atributos(X,destreza)), nl.
+opc_pergunta_2(X,3) :- write(atributos(X,vitalidade)), nl.
+opc_pergunta_2(X,4) :- write(atributos(X,destreza)), nl.
